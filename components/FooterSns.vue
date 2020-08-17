dc161a110d6726476c9a8ecd90a88b4975b4f9a1
Takuya Tomioka <t-tomioka@v-trade.co.jp>
2020-08-17 16:05:56 +0900

    Create Footer

---
+++ components/FooterSns.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <section>
+				<div class="w-full flex items-center pt-8 text-3xl">
+					<div class="w-1/3 inline-block">
+						<a target="_blank" href=" https://www.facebook.com/AVIOTTAIWAN" class="flex items-center justify-center">
+							<img src="@/assets/img/top/FB.jpg" alt="Facebook Account" />
+						</a>
+					</div>
+					<div class="w-1/3 inline-block">
+						<a target="_blank" href="https://www.instagram.com/aviottaiwan/" class="flex items-center justify-center">
+							<img src="@/assets/img/top/IG.jpg" alt="Instagram Account" />
+						</a>
+					</div>
+					<div class="w-1/3 inline-block">
+						<a target="_blank" href="https://www.youtube.com/channel/UC7o4le0fhW4YDoDpgDsdULw" class="flex items-center justify-center">
+							<img src="@/assets/img/top/YOUTUBE.jpg" alt="Youtube Account" />
+						</a>
+					</div>
+				</div>
+			</section>
+  </div>
+</template>
---
+++ components/TheFooter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <footer>
+      <FooterSns />
+      <p>&copy;AVIOT</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import FooterSns from "@/components/FooterSns";
+export default {
+  components:{
+    FooterSns,
+  }
+}
+</script>
+
+<style lang="scss">
+footer{
+  background-color: black;
+  color: white;
+  text-align: center;
+}
+</style>
